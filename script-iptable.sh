b6469063efe0d96460d9239618e12931d7653b8b
jngumy <56744971+jngumy@users.noreply.github.com>
2020-06-08 19:29:25 -0300

    Add files via upload

--- script-iptable.sh
+++ script-iptable.sh
@@ -1,136 +1,92 @@
 #!/bin/bash
-##############USMAN AKRAM "FA05-BTN-005" (~*Lucky*~) BTN-6########
-######COMSATS INSTITUTE OF INFORMATION TECHNOLOGY - ABBOTTABAD####
-echo -e "****************Welcome*************"
-###############################IPTABLE SERVICES PROGRAM BEGINS HERE###############################
-checkstatus()
- {
-  opt_checkstatus=1
- while [ $opt_checkstatus != 7 ]
-      do
-       clear
-  #echo -e "\nChoose the Option Bellow!!!\n
-  echo -e "\n\t*****Note: Save your Iptables before stop/Restart the iptables Services*****\n"
-  echo -e "   1. Save the iptables\n
-   2. Status of Iptables\n
-   3. Start iptables Services\n
-   4. Stop iptables Services\n
-   5. Restart iptable Services\n
-   6. Flush iptables (**Use Carefully_it will remove all the rules from iptables**)\n
-   7. Go back to Main Menu"
-  read opt_checkstatus
-  case $opt_checkstatus in
-   1) echo -e "*******************************************************\n" 
-               /etc/init.d/iptables save 
-      echo -e "\n*******************************************************\n"
-    echo -e "Press Enter key to Continue..."
-    read temp;;
-   2) echo -e "*******************************************************\n"
-               /etc/init.d/iptables status 
-      echo -e "*******************************************************"
-                                echo -e "Press Enter key to Continue..."
-                                     read temp;;
-   3) echo -e "*******************************************************\n"  
-               /etc/init.d/iptables start 
-      echo -e "*******************************************************\n"
-                                 echo -e "Press Enter key to Continue..."
-                                       read temp;;
-   
-   4) echo -e "*******************************************************\n"
-               /etc/init.d/iptables stop
-      echo -e "*******************************************************\n"
-                                echo -e "Press Enter key to Continue..."
-                                     read temp;;
-     
-             5) echo -e "*******************************************************\n"
-                      /etc/init.d/iptables restart 
-      echo -e "*******************************************************\n"
-                                echo -e "Press Enter key to Continue..."
-                                     read temp;;
-   6) iptables -F 
-   echo -e "*******************************************************"
-   echo -e "All the Rules from the Iptables are Flushed!!!"
-   echo -e "*******************************************************\n"
-                                echo -e "Press Enter key to Continue..."
-                                 read temp;;
-   7) main;;
-   *) echo -e "Wrong Option Selected!!!"
-  esac
- done
- }
-###############################BUILD FIREWALL PROGRAM BEGINS FROM HERE############################### 
+
+echo -e "--------Script IPTABLES--------"
+
+############################# Programa de Firewall y reglas############################### 
 buildfirewall()
  {
-  ###############Getting the Chain############
-  echo -e "Using Which Chain of Filter Table?\n
+  ###############Obteniendo la cadena############
+  echo -e "Seleccione un tipo de cadena\n
   1. INPUT
   2. OUTPUT
-  3. Forward"
+  3. FORWARD
+  4. PREROUTING
+  5. POSTROUTING"
   read opt_ch
   case $opt_ch in
    1) chain="INPUT" ;;
    2) chain="OUTPUT" ;;
    3) chain="FORWARD" ;;
-   *) echo -e "Wrong Option Selected!!!"
+   4) chain="PREROUTING" ;;
+   5) chain="POSTROUTING";;
+   *) echo -e "Opción inválida"
   esac
  
-  #########Getting Source IP Address##########
-  #Label
+  #########Obteniendo direccion IP fuente##########
+ 
    
   echo -e "
-  1. Firewall using Single Source IP\n
-  2. Firewall using Source Subnet\n
-  3. Firewall using for All Source Networks\n"
+  1. Regla usando direccion IP de origen única\n
+  2. Regla usando direccion subred de origen\n
+  3. Regla usando todas las redes de origen\n"
   read opt_ip
-   
+ 
   case $opt_ip in
-   1) echo -e "\nPlease Enter the IP Address of the Source"
+   1) echo -e "\nIntroduzca la IP origen"
    read ip_source ;;
-   2) echo -e "\nPlease Enter the Source Subnet (e.g 192.168.10.0/24)"
+   2) echo -e "\nIntroduzca la IP de la subred de origen (Por ejemplo 192.168.10.0/24)"
    read ip_source ;;
    3) ip_source="0/0" ;;
    #4) ip_source = "NULL" ;;
-   *) echo -e "Wrong Option Selected"
+   *) echo -e "Opción errónea"
   esac
-  #########Getting Destination IP Address##########
+  #########Obteniendo la direccion IP destino##########
    echo -e "
-  1. Firewall using Single Destination IP\n
-                2. Firewall using Destination Subnet\n
-         3. Firewall using for All Destination Networks\n"
+  1. Regla usando direccion IP destino única\n
+  2. Regla usando direccion de subred destino\n
+  3. Regla usando todas las redes de destino\n"
   
      read opt_ip
               case $opt_ip in
-        1) echo -e "\nPlease Enter the IP Address of the Destination"
+        1) echo -e "\nIntroduzca la IP origen"
                      read ip_dest ;;
-               2) echo -e "\nPlease Enter the Destination Subnet (e.g 192.168.10.0/24)"
+        2) echo -e "\nIntroduzca la IP de la subred destino(Por ejemplo 192.168.10.0/24)"
                      read ip_dest ;;
-               3) ip_dest="0/0" ;;
+        3) ip_dest="0/0" ;;
         #4) ip_dest = "NULL" ;;
-               *) echo -e "Wrong Option Selected"
+        *) echo -e "Opcion errónea"
        esac
-       ###############Getting the Protocol#############
+
+
+
+       ###############Obteniendo el Protocolo#############
        echo -e "
-       1. Block All Traffic of TCP
-       2. Block Specific TCP Service
-       3. Block Specific Port
-       4. Using no Protocol"
+       1. Aplicado a todo el tráfico TCP
+       2. Aplicado a todo el trafico UDP
+       3. Aplicado a un puerto espécifico TCP
+       4. Aplicado a un puerto especifico UDP 
+       5. Sin protocolo"
        read proto_ch
        case $proto_ch in
         1) proto=TCP ;;
-        2) echo -e "Enter the TCP Service Name: (CAPITAL LETTERS!!!)"
-       read proto ;;
-        3) echo -e "Enter the Port Name: (CAPITAL LETTERS!!!)" 
-       read proto ;;
-        4) proto="NULL" ;;
-        *) echo -e "Wrong option Selected!!!"
+        2) proto=UDP ;;
+        3) echo -e "Ingresar el numero de puerto TCP:"
+           proto=TCP 
+           read puerto ;;
+        4) echo -e "Ingresar el numero de puerto UDP:"
+	   proto=UDP
+	   read puerto;;
+	5) proto="NULL" 
+	   puerto="NULL";;
+        *) echo -e "Opcion errónea"
        esac
  
-       #############What to do With Rule############# 
-       echo -e "What to do with Rule?
-       1. Accept the Packet
-       2. Reject the Packet
-       3. Drop the Packet
-       4. Create Log"
+       #############Accion con la regla############# 
+       echo -e "¿Qué acción realizar con la regla?
+       1. Aceptar el paquete
+       2. Rechazar el paquete
+       3. Dropear el paquete
+       4. Crear Log"
        read rule_ch
        case $rule_ch in 
         1) rule="ACCEPT" ;;
@@ -139,30 +95,56 @@ buildfirewall()
         4) rule="LOG" ;;
        esac
 ###################Generating the Rule####################
-echo -e "\n\tPress Enter key to Generate the Complete Rule!!!"
+echo -e "\n\tPresione cualquier regla para generar la regla:"
 read temp
-echo -e "The Generated Rule is \n"
-if [ $proto == "NULL" ]; then
+echo -e "La regla generada es: \n"
+if [ $proto == "NULL" ] && [ $puerto == "NULL" ]; then
  echo -e "\niptables -A $chain -s $ip_source -d $ip_dest -j $rule\n"
  gen=1
 else
- echo -e "\niptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule\n"
- gen=2
-fi 
-echo -e "\n\tDo you want to Enter the Above rule to the IPTABLES? Yes=1 , No=2"
-read yesno
-if [ $yesno == 1 ] && [ $gen == 1 ]; then
- iptables -A $chain -s $ip_source -d $ip_dest -j $rule
-else if [ $yesno == 1 ] && [ $gen == 2 ]; then
- iptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule         
-   
-else if [ $yesno == 2 ]; then
+	if [ $proto != "NULL" ] && [ $puerto == "NULL"]; then
+	   echo -e "\niptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule\n"
+	   gen=2
+
+        else
+	    echo -e "\niptables -A $chain -s $ip_source -d $ip_dest -p $proto --dport $puerto -j $rule\n"
+ 	    gen=3
+	fi
  
- main
-fi
-fi
+fi 
+echo -e "\n\tDesea ingresar la regla anterior a IPTABLES? Si=1 , No=2"
+read yesno
+
+if [ $yesno == 1 ]; then
+  if [ $gen == 1 ]; then
+     iptables -A $chain -s $ip_source -d $ip_dest -j $rule
+  else
+	if [ $gen == 2 ]; then
+	  iptables -A $chain -s $ip_source -d $ip_dest -p $proto -j $rule
+	else
+	  iptables -A $chain -s $ip_source -d $ip_dest -p $proto --dport $puerto -j $rule
+       fi
+else
+  main
 fi
 }
+
+
+
+########
+#if [ $yesno == 1 ] && [ $gen == 1 ]; then
+# iptables -A $chain -s $ip_source -d $ip_dest -j $rule
+#else if [ $yesno == 1 ] && [ $gen == 2 ]; then
+# iptables -A $chain -s $ip_source -d $ip_dest -p $proto  -j $rule         
+
+#else if [ $yesno == 2 ]; then
+
+######### 
+# main
+#fi
+#fi
+#fi
+#}
       
 main()
 {
@@ -173,26 +155,23 @@ main()
  opt_main=1
  while [ $opt_main != 4 ]
  do
-echo -e "/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\n" 
-#############Check Whether the iptables installed or not############ 
- echo -e "\t*****Main Menu*****\n
- 1. Check Iptables Package\n
- 2. Iptables Services\n
- 3. Build Your Firewall with Iptables\n
- 4. Exit"
+
+#############Menu principal############ 
+ echo -e "\t---------Menú Principal---------\n
+ 
+ 1. Iptables Services\n
+ 2. Agregar una regla al firewall\n
+ 3. Salir"
  read opt_main
  case $opt_main in
-  1) echo -e "******************************"
-    rpm -q iptables 
-     echo -e "******************************" ;;
-  2) checkstatus ;;
-  3) buildfirewall ;;
-  4) exit 0 ;;
-  *) echo -e "Wrong option Selected!!!"
+  1) checkstatus ;;
+  2) buildfirewall ;;
+  3) exit 0 ;;
+  *) echo -e "Opcion incorrecta"
  esac
 done
 else
- echo -e "You Must be the ROOT to Perfom this Task!!!"
+ echo -e "Debes ser root para ejecutar este script"
 fi
 }
 main
