b4453de3bbe4bc8a4c6bad26440eb76266ae90b4
jngumy <56744971+jngumy@users.noreply.github.com>
2020-06-09 15:04:35 -0300

    Update dhcp-leases.sh

--- dhcp-leases.sh
+++ dhcp-leases.sh
@@ -1,31 +1,9 @@
-awk 'BEGIN{
-    while( (getline line < "maclist") > 0){
-        mac[line]
-    }
-    RS="}"
-    FS="\n"
+# /var/lib/dhcp/dhcpd.leases
+#!/bin/bash
+function f_mac_to_ip {
+
+parseResult=$(cat /var/lib/dhcp/dhcpd.leases |  egrep -o 'lease.*{|ethernet.*;' | awk '{print $2}' | xargs -n 2 | cut -d ';' -f 1  | grep $1 | awk '{print $1}')
+    echo "$parseResult"
 }
-/lease/{
-    for(i=1;i<=NF;i++){
-        gsub(";","",$i)
-        if ($i ~ /lease/) {
-            m=split($i, IP," ")
-            ip=IP[2]
-        }
-        if( $i ~ /hardware/ ){
-            m=split($i, hw," ")
-            ether=hw[3]
-        }
-        if ( $i ~ /client-hostname/){
-            m=split($i,ch, " ")
-            hostname=ch[2]
-        }
-        if ( $i ~ /uid/){
-            m=split($i,ui, " ")
-            uid=ui[2]
-        }
-    }
-    if ( ether in mac ){
-        print "ip: "ip " hostname: "hostname " ether: "ether " uid: "uid
-    }
-} ' /var/lib/dhcp/dhcpd.leases
+
+f_mac_to_ip
