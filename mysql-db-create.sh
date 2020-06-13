f865ce52efd62eefe3b269bc65631a3e08a34769
jngumy <56744971+jngumy@users.noreply.github.com>
2020-06-13 01:06:07 -0300

    Add files via upload

---
+++ mysql-db-create.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+ok() { echo -e '\e[32m'$1'\e[m'; } # Green
+
+EXPECTED_ARGS=3
+E_BADARGS=65
+MYSQL=`which mysql`
+ 
+Q1="CREATE DATABASE IF NOT EXISTS $1;"
+Q2="GRANT ALL ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
+Q3="FLUSH PRIVILEGES;"
+SQL="${Q1}${Q2}${Q3}"
+ 
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: $0 dbname dbuser dbpass"
+  exit $E_BADARGS
+fi
+ 
+$MYSQL -uroot -p -e "$SQL"
+
+ok "Base de datos $1 y usuario  $2 creados exitosamente con password $3"
